911564c2cc47cb142682524ef7226f081e0f26f5
sunghoon <sunghoon@bharvest.io>
2021-05-03 00:11:57 +0900

    fix: add mobile reactiveness of banner

--- src/components/Banners/HeaderTopBanner.tsx
+++ src/components/Banners/HeaderTopBanner.tsx
@@ -3,19 +3,20 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
     width: 100%;
-    height: 28px;
+    height: 100%;
+    overflow: hidden;
     position: relative;
     color: #fff;
 .banner {
-    position:absolute;
+    position:relative;
     display: flex;
     justify-content: space-between;
     align-items: center;
     top: 0;
     left: 0;
-    height: 28px;
+    /* height: 28px; */
     width: 100%;
-    padding: 0 20px;
+    padding: 4px 20px;
 
     .banner-close {
         font-size: 20px;
@@ -26,15 +27,23 @@ const Wrapper = styled.div`
         color: #fff !important;
     }
 }
+
+.background {
+    position: absolute;
+    width: 100%;
+    height: 80px;
+    top: 0;
+    left: 0;
+}
   
 .bg {
   animation:slide 3s ease-in-out infinite alternate;
   background-image: linear-gradient(-60deg,#cb4df5 50%,#e26b5d 50%);
   bottom:0;
-  height: 28px;
+  height: 80px;
   left:-50%;
   opacity:.5;
-  position:fixed;
+  position:absolute;
   right:-50%;
   top:0;
   z-index:-1;
@@ -76,9 +85,11 @@ function HeaderTopBanner() {
     const [isClose, setIsClose] = React.useState(false)
     return (
         <Wrapper style={{ display: isClose ? 'none' : 'unset' }}>
-            <div className="bg"></div>
-            <div className="bg bg2"></div>
-            <div className="bg bg3"></div>
+            <div className="background">
+                <div className="bg"></div>
+                <div className="bg bg2"></div>
+                <div className="bg bg3"></div>
+            </div>
             <div className="banner">
                 <div className="banner-content">Soon™️, There will be Sudden Price Change 📈  and APY Boosting 🚀  (More info at <a href="https://gravitydex.io" target="_blank" rel="noopener noreferrer">Here</a>)</div>
                 <div onClick={() => {
