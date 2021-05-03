7eac5b798ae4e1d66a064853883a9c190210764b
sunghoon <sunghoon@bharvest.io>
2021-05-03 17:51:39 +0900

    feat: apply performance optimization (browser level)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -87,9 +87,31 @@ function App() {
     }
 
     dispatch(requestQueryParams())
+
+    const visibilityChecker = (function(){
+      let stateKey, eventKey, keys = {
+          hidden: "visibilitychange",
+          webkitHidden: "webkitvisibilitychange",
+          mozHidden: "mozvisibilitychange",
+          msHidden: "msvisibilitychange"
+      };
+      for (stateKey in keys) {
+          if (stateKey in document) {
+              eventKey = keys[stateKey];
+              break;
+          }
+      }
+      return function() {
+          // if (c) document.addEventListener(eventKey, c);
+          return !document[stateKey];
+      }
+  })();
+
     setInterval(() => {
-      dispatch(requestQueryLiquidityPools())
-      setCoinPrices()
+      if(visibilityChecker()) {
+        dispatch(requestQueryLiquidityPools())
+        setCoinPrices()
+      } 
     }, 7000)
 
     dispatch(requestQueryLiquidityPools())
